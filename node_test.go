25aa25aecd0b94b82fcebe83c490ce8808a6d032
Chris Campbell <chrs.cmpbl@gmail.com>
2023-01-27 14:08:43 -0700

    sub command tests

--- node_test.go
+++ node_test.go
@@ -35,13 +35,42 @@ func TestNodeBuild(t *testing.T) {
 		verifyNode(t, buildNodes(reflect.ValueOf(&foo{})),
 			0, 0, reflect.ValueOf(foo{}).Type().String(), reflect.ValueOf(&foo{}).Type().String(), true, true)
 	})
+	t.Run("sub command", func(t *testing.T) {
+		type sub struct {
+		}
+		type foo struct {
+			Sub sub
+		}
+		node := buildNodes(reflect.ValueOf(&foo{}))
+		verifyNode(t, node,
+			1, 0, reflect.ValueOf(foo{}).Type().String(), reflect.ValueOf(&foo{}).Type().String(), false, false)
+		verifyNode(t, node.Commands["sub"],
+			0, 0, reflect.ValueOf(sub{}).Type().String(), reflect.ValueOf(&sub{}).Type().String(), false, false)
+	})
+	t.Run("multiple sub commands", func(t *testing.T) {
+		type sub1 struct {
+		}
+		type sub2 struct {
+		}
+		type foo struct {
+			Sub1 sub1
+			Sub2 sub2
+		}
+		node := buildNodes(reflect.ValueOf(&foo{}))
+		verifyNode(t, node,
+			2, 0, reflect.ValueOf(foo{}).Type().String(), reflect.ValueOf(&foo{}).Type().String(), false, false)
+		verifyNode(t, node.Commands["sub1"],
+			0, 0, reflect.ValueOf(sub1{}).Type().String(), reflect.ValueOf(&sub1{}).Type().String(), false, false)
+		verifyNode(t, node.Commands["sub2"],
+			0, 0, reflect.ValueOf(sub2{}).Type().String(), reflect.ValueOf(&sub2{}).Type().String(), false, false)
+	})
 }
 
-func verifyNode(t *testing.T, node *Node, commands, options int, tt, vt string, optsValid, runValid bool) {
+func verifyNode(t *testing.T, node *Node, commandCount, optionsCount int, tt, vt string, optsValid, runValid bool) {
 	t.Helper()
 	assert.NotNil(t, node)
-	assert.Len(t, node.Commands, 0)
-	assert.Len(t, node.Options, 0)
+	assert.Len(t, node.Commands, commandCount)
+	assert.Len(t, node.Options, optionsCount)
 	assert.Equal(t, tt, node.Type.String())
 	assert.Equal(t, vt, node.Value.Type().String())
 	assert.Equal(t, optsValid, node.Opts.IsValid())
